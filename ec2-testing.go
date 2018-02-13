ea64ec7ad21ba3fade1047fe4ae029e733fc4cc8
Dmitrii Makarov <dmitrii_makarov@epam.com>
2018-02-13 18:28:30 +0300

    added function get responce body

--- ec2-testing.go
+++ ec2-testing.go
@@ -17,27 +17,39 @@ func hostname() string {
 	return "localhost"
 }
 
-func getMetaData(meta_data_item string) string {
+func getResponceBody(url string) string {
 	timeout := time.Duration(1 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
 	}
-	resp, err := client.Get("http://169.254.169.254/latest/meta-data/" + meta_data_item + "/")
+	resp, err := client.Get(url)
 	if err != nil {
-		fmt.Println("Can't get aws meta data item: " + meta_data_item)
-		return "can't get " + meta_data_item
+		fmt.Println("Can't get URL: " + url)
+		return "can't get URL" + url
 	}
 	defer resp.Body.Close() // Close body only if response non-nil
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
 		if err2 != nil {
-			fmt.Println("Can't get responce Body for meta data item: " + meta_data_item)
-			return "can't get " + meta_data_item
+			fmt.Println("Can't get responce Body for URL: " + url)
+			return "can't get  responce Body for URL: " + url
 		}
 		bodyString := string(bodyBytes)
 		return bodyString
 	}
-	return "Status is Not OK for " + meta_data_item
+	return "Status is Not OK for URL: " + url
+}
+
+// func collectMetaURLs(start_url string) []string {
+// 	timeout := time.Duration(1 * time.Second)
+// 	client := http.Client{
+// 		Timeout: timeout,
+// 	}
+// 	resp, err := client.Get(start_url + "/")
+// }
+
+func getMetaData(meta_data_item string) string {
+	return getResponceBody("http://169.254.169.254/latest/meta-data/" + meta_data_item + "/")
 }
 
 func main() {
